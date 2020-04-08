f3f182b9d583677a8687ad61c18319e48c08e389
Zach Brooks <hcaz1444@gmail.com>
2020-04-08 17:08:51 -0600

    Fixed External Link Serializer
    Fixed External Link Serializer

--- web/src/components/BlockContent.vue
+++ web/src/components/BlockContent.vue
@@ -38,18 +38,18 @@ export default {
             const { slug = {} } = mark
             const href = `/${slug.current}`
             return <a href={href}>{children}</a>
+          },
+          link: ({ mark, children }) => {
+            // Read https://css-tricks.com/use-target_blank/
+            const { blank, href } = mark
+            return blank ? (
+              <a href={href} target="_blank" rel="noopener">
+                {children}
+              </a>
+            ) : (
+              <a href={href}>{children}</a>
+            )
           }
-        },
-        link: ({ mark, children }) => {
-          // Read https://css-tricks.com/use-target_blank/
-          const { blank, href } = mark
-          return blank ? (
-            <a href={href} target="_blank" rel="noopener">
-              {children}
-            </a>
-          ) : (
-            <a href={href}>{children}</a>
-          )
         }
       }
     }
